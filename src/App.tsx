185a3d50c845b13b72a1a582dc0c60e390c064cb
Patricia Eliseu <patyeliseu4@gmail.com>
2025-11-15 21:38:45 -0300

    add projeto

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
+
+
+
+function App() {
+ 
+
+  return (
+   <>
+   <BrowserRouter>
+    <Navbar />
+    <div className="min-h-[80vh]">
+    <Routes>
+      <Route path= "/" element={<Home/>} />
+      <Route path= "/home" element={<Home/>} />
+      <Route path= "/login" element={<Login/>} />
+      <Route path= "/cadastro" element={<Cadastro/>} />
+    </Routes>
+    </div>
+    <Footer/>
+   </BrowserRouter>
+   </>
+  )
+}
+
+export default App
